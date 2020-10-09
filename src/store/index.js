e16e593c6ce72571ecaa395cae61e669dcb2a47a
NorbertLuszkiewicz <norbertluszkiewicz00@gmail.com>
2020-10-09 17:46:58 +0200

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-underscore-dangle */
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { bookListReducer } from 'reducers';
+
+const reducer = combineReducers({
+  bookList: bookListReducer,
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+export default store;
