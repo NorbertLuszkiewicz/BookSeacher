f1ff4755ae0aa3e638343dd79332a28629ad16f6
NorbertLuszkiewicz <norbertluszkiewicz00@gmail.com>
2020-10-10 18:09:02 +0200

    Add author and publisher logic

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,12 +4,19 @@ export const FIND_BOOK_REQUEST = 'FIND_TITLE_REQUEST';
 export const FIND_BOOK_SUCCESS = 'FIND_TITLE_SUCCESS';
 export const FIND_BOOK_FAILURE = 'FIND_TITLE_FAILURE';
 
-const SERVER_URL = 'https://www.googleapis.com/books/v1/';
+const API_URL = 'https://www.googleapis.com/books/v1/';
 
-export const bookList = (title, author, launguage, publishedDate) => async dispatch => {
+export const bookList = (title, author, publisher) => async dispatch => {
   try {
     dispatch({ type: FIND_BOOK_REQUEST });
-    const { data } = await axios.get(`${SERVER_URL}volumes?q=${title}`);
+    const bookTitle = title ? `intitle:${title}` : 'intitle:NoSearchResults';
+    const bookAuthor = author ? `inauthor:${author}` : '';
+    const bookPublisher = publisher ? `inpublisher:${publisher}` : '';
+    const results = `&maxResults=12`;
+
+    const { data } = await axios.get(
+      `${API_URL}volumes?q=${bookTitle}+${bookAuthor}+${bookPublisher}${results}`,
+    );
     dispatch({ type: FIND_BOOK_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: FIND_BOOK_FAILURE, payload: error.message });
