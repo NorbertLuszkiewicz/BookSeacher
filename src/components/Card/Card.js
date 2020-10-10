4e879e90ab3316a0be041fcb48e5b29bb8ce5829
NorbertLuszkiewicz <norbertluszkiewicz00@gmail.com>
2020-10-10 18:22:54 +0200

    Change look and add proptypes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Wrapper = styled.div`
   width: 100%;
-  border: 1px solid green;
+  border: 4px solid #fff77a;
   border-radius: 10px;
   padding: 10px;
   text-align: center;
@@ -18,16 +19,43 @@ const Title = styled.h1`
 const Paragraph = styled.p`
   font-size: ${({ theme }) => theme.fontSize.s};
   margin: 5px 0;
+  text-overflow: clip;
+`;
+const ParagraphWrapper = styled.div`
+  height: 62px;
+  width: 100%;
+  overflow: hidden;
 `;
 
-const Card = ({ title, author, image, language, publishedDate }) => (
-  <Wrapper>
-    <img src={image} alt={title} />
-    <Title>{title}</Title>
-    <Paragraph>Author: {author}</Paragraph>
-    <Paragraph>language: {language}</Paragraph>
-    <Paragraph>published: {publishedDate}</Paragraph>
-  </Wrapper>
-);
+const Img = styled.img`
+  height: 192px;
+`;
+
+const Card = ({ title, image, description }) => {
+  const noBookImage = 'https://cdn.pixabay.com/photo/2014/04/02/16/21/book-307045_960_720.png';
+
+  return (
+    <Wrapper>
+      <Img src={image.thumbnail || noBookImage} alt={title} />
+      <Title>{title}</Title>
+      {description && (
+        <ParagraphWrapper>
+          <Paragraph>{description} </Paragraph>
+        </ParagraphWrapper>
+      )}
+    </Wrapper>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  description: PropTypes.string,
+};
+
+Card.defaultProps = {
+  description: '',
+  image: '',
+};
 
 export default Card;
