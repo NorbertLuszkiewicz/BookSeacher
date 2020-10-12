fdd7688007f09320d40972f0709c85871d0af5d0
NorbertLuszkiewicz <norbertluszkiewicz00@gmail.com>
2020-10-12 14:56:03 +0200

    Initial commit

---
+++ src/components/Button/Button.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  position: relative;
+  width: 250px;
+  font-size: ${({ theme }) => theme.fontSize.s};
+  padding: 7px;
+  background: none;
+  border-radius: 10px;
+  font-weight: bold;
+  overflow: hidden;
+
+  ::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #64b5f6;
+
+    transform: translateY(100%);
+    z-index: -1;
+    transition: transform 0.3s ease;
+  }
+
+  :hover::before {
+    transform: translateY(0);
+  }
+`;
+
+export default Button;
